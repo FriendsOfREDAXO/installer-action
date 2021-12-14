80be48f000cf85923cc1c5ae4636c5489434c1ed
Marcel Kuhmann <marcel@dermarcel.eu>
2021-12-14 23:27:26 +0100

    build for test

--- dist/index.js
+++ dist/index.js
@@ -40330,6 +40330,7 @@ async function zip(cacheFile, addonDir, addonName, ignoreList) {
     archive.pipe(output);
     core.info(`Creating archive in ${cacheFile}`);
     core.info(`Using installer_ignore ${JSON.stringify(ignoreList)}`);
+    ignoreList.push('.git/*');
     archive.on('entry', entry => {
         core.info(`Adding to zip: ${entry.name}`);
     });
