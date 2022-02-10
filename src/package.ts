9b2fe31601ea436200a6d157695802c07bc341a4
Marcel Kuhmann <marcel@dermarcel.eu>
2022-02-10 18:27:19 +0100

    improve yaml handling (#18)

--- src/package.ts
+++ src/package.ts
@@ -15,7 +15,31 @@ export async function readPackageYml(addonDir: string): Promise<PackageYml> {
     const packageYmlPath = path.join(addonDir, '/', 'package.yml');
     Core.info(`Parsing yaml from ${packageYmlPath}`);
     const packageYmlString = fs.readFileSync(packageYmlPath, {encoding: 'utf8'});
-    return YAML.parse(packageYmlString);
+
+    try {
+        return YAML.parse(packageYmlString, {
+            prettyErrors: true,
+        });
+    } catch (originalError) {
+        Core.error(`Failed to parse yaml from ${packageYmlPath}. Try to fix it automatically.`);
+
+        // try to escape "perm" value
+        const fixedPackageYmlString = packageYmlString.replace(/(perm:[.\s\n\r\t]*)([a-z0-9_]+\[[a-z0-9_]+\])/ig, `$1'$2'`);
+
+        // second try with escaped "perm" value
+        try {
+            const parsedYaml = YAML.parse(fixedPackageYmlString, {
+                prettyErrors: true,
+            });
+            Core.info(`Successfully parsed yaml with fixed values.`);
+
+            return parsedYaml;
+        } catch (_) {
+            // failed to parse yaml with fixed values
+            Core.error(`Automatically fixed yaml failed. Please fix it manually.`);
+            throw originalError;
+        }
+    }
 }
 
 export function md5_file(file: string): Promise<string> {
